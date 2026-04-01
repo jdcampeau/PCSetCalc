3f15fdb3e3eea1bc1f12605787643c48a443e8ab
jdcampeau <jdcgithub@duck.com>
2026-04-01 06:13:10 -0500

    refactoring duodec.py, writing funcitons in function.py

--- duodec.py
+++ duodec.py
@@ -1,4 +1,11 @@
 import itertools
+from functions import get_intervals, get_prime_form
+
+def get_set(idx_list):
+    set = []
+    for idx in idx_list:
+
+
 def main():
     print("Checking all arrangements of PC's 1-11 relative to PC 0...")
     total_arrangements = 0
@@ -10,36 +17,31 @@ def main():
         total_arrangements += 1
         perm_list = list(perm)
         all_pcs = c.extend(perm_list)
-        set1 = [all_pcs[0], all_pcs[1], all_pcs[3], all_pcs[4], all_pcs[5], all_pcs[9]]
-        set2 = 
-        set3 = 
-        set4 = 
-        set5 =
-        set6 = 
-        set7 = 
-        set8 = 
-        set9 = 
-        set10 = 
-        set11 = 
-        set12 = 
-        sets = [set1, set2, set3, set4, set5, set6, set7, set8, set9, set10, set11, set12]
-        prime_forms = []
+        set_indices = [[0, 1, 3, 4, 5, 9], [1, 0, 3, 6, 7, 9], [2, 3, 5, 7, 10, 11], [3, 0, 1, 2, 5, 7],
+                       [4, 0, 5, 8, 9, 10], [5, 0, 2, 3, 4, 10], [6, 1, 7, 8, 9, 11], [7, 1, 2, 3, 6, 11],
+                       [8, 4, 6, 9, 10, 11], [9, 0, 1, 4, 6, 8], [10, 2, 4, 5, 8, 11], [11, 2, 6, 7, 8, 10]]
+        sets = []
+        for indices in set_indices:
+            pc_set = []
+            for idx in indices:
+                pc_set.append(all_pcs[idx])
+            sets.append(pc_set)
+        unique_prime_forms = []
         for st in sets:
             st.sort()
-            int_seq = f"{st[1] - st[0]}{st[2] - st[1]}{st[3] - st[2]}{st[4] - st[3]}{st[5] - st[4]}{12 - st[5]}"
+            int_seq = get_intervals(st)
             #code here: if int_seq matches column imported from hexintseqs: reordered_set = something
             #add code for inversion check - if conditions for inversion are met:
                                                 #prime_form = xyz
                                             #else:
                                                 #prime_form = abc
-            #add code to replace any occurence of "10" with "T"
-            #add code to convert prime_form to string (prime_str)
-            if prime_str not in unique_prime_forms:
-                unique_prime_forms.append(prime_str)
-            all_prime_forms.append(prime_str)
-        pc_string = ",".join(all_pcs)
+            prime_form = get_prime_form(inversion_checked)
+            if prime_form not in unique_prime_forms:
+                unique_prime_forms.append(prime_form)
         if len(unique_prime_forms) == 12:
             twelves += 1
-            twelves_dict[pc_string] = all_prime_forms
+            pc_string = ",".join(all_pcs)
+            unique_string = ",".join(unique_prime_forms)
+            twelves_dict[pc_string] = unique_string
     print(f"Out of {total_arrangements / 5} unique arrangements, there are")
-    print(f"
+    print(f"{twelves} arrangements that yield twelve unique prime forms.")
