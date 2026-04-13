cb1d95af1473531a45f9ca26002a1317cd862fec
jdcampeau <jdcgithub@duck.com>
2026-04-13 06:23:27 -0500

    added test files, will write tests

---
+++ test_functions.py
@@ -0,0 +1,14 @@
+import unittest
+
+from functions import find_pcs_notes, find_pcs_booleans, get_intervals, rotate_intervals, get_best_normal_order, get_prime_form, pfrom_from_booleans, pfrom_from_notes
+
+class TestFunctions(unittest.TestCase):
+    def #add test to find intervals from boolean input
+
+    def # add test to find intervals from text input
+
+    def #add test to find prime form from boolean input
+
+    def #add test to find prime form from text input
+
+    #write each test to compare outputs for a list of chords
