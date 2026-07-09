cd677b7102a342a4113f6644970bc7c2be7ba8e9
jdcampeau <jdcgithub@duck.com>
2026-07-09 07:27:56 -0500

    tested and updated main.py to ensure tkinter working as expected

--- src/main.py
+++ src/main.py
@@ -4,17 +4,17 @@ from calculator import *
 
 class App(tk.Tk):
     def __init__(self):
-        super().__init()
+        super().__init__()
         self.title("Musical Set Theory: Prime Form Calculator")
         self.geometry("600x300")
 
-        self.label = tk.label(root, text="Enter your name:", font=("Arial", 12))
+        self.label = tk.Label(self, text="Enter your name:", font=("Arial", 12))
         self.label.pack(pady=10)
 
-        self.entry = tk.Entry(root, font=("Arial", 12))
+        self.entry = tk.Entry(self, font=("Arial", 12))
         self.entry.pack(pady=5)
 
-        self.button = tk.Button(root, text="Calculate", command=handle_click)
+        self.button = tk.Button(self, text="Calculate", command=self.handle_click)
         self.button.pack(pady=10)
 
     def handle_click(self):
